3f031b74d80b8fc209b5c6987ce24314fc86d96f
Khusharth <khusharth19@gmail.com>
2020-05-16 02:51:21 +0530

    Added API Key in .env file

--- src/api/unspash.js
+++ src/api/unspash.js
@@ -1,11 +1,10 @@
 // Contains code related to configuration of Axios
-import axios from 'axios';
+import axios from "axios";
 
 // Creating new instance of axios client with some default properties
 export default axios.create({
-    baseURL: 'https://api.unsplash.com/',
+    baseURL: "https://api.unsplash.com/",
     headers: {
-        Authorization: 'Client-ID 367NLzgk7_PfmIiXK7mYelqSSJS93s1xRv0O6hB33IM'
-    }
+        Authorization: `${process.env.REACT_APP_API_KEY}`,
+    },
 });
-
